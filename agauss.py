ef152612fe3a073b8a5d04ef8a44724610a3fd77
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-04-02 22:52:55 +0300

    Finished the agauss.py module

--- agauss.py
+++ agauss.py
@@ -2,6 +2,7 @@
 # just calls calcGaussianLine
 
 from Numerics import KEV_TO_A
+import calcLines
 
 def agauss(energyArray, params, spectrumNumber, fluxArray, fluxErrArray, initString):
 
@@ -14,6 +15,14 @@ def agauss(energyArray, params, spectrumNumber, fluxArray, fluxErrArray, initStr
 	for i in range(0, nE):
 		angstromArray.append(KEV_TO_A / energyArray[nE - 1 - i])
 
+	crtLevel = 1.0e-6
 	lineParams = list(params[1])
+
 	angstromFluxArray = []
+	calcLines.calcLine(angstromArray, params[0], lineParams, 1.0, crtLevel, 0, False, angstromFluxArray)
+
+	fluxArray.resize(energyArray.size()-1);
+	for i in range(0, nE - 1):
+		fluxArray[i] = angstromFluxArray[nE - 2 - i]
+	fluxErrArray = []
 
