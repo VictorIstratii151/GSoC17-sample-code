da6a421286f42883babc2d02b4928620470832cc
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-04-02 20:57:07 +0300

    removed all the monospaces

--- calcLines.py
+++ calcLines.py
@@ -10,27 +10,26 @@ def calcLine(energyArray, ecenter, lineParams, lineflux, crtLevel, lineShape, qs
 
 def calcManyLines(energyArray, ecenterArray, lineParamsArray, linefluxArray, crtLevel, lineShape, qspeedy, fluxArray):
 	# Find the bins containing the line centers. This assumes that the ecenter
- 	# array is in increasing order of energy. If the center energy falls outside
- 	# the energy array then lineCenterBin will be set to -1. Otherwise
- 	# energyArray[lineCenterBin] < ecenterArray <= energyArray[lineCenterBin+1]
+	# array is in increasing order of energy. If the center energy falls outside
+	# the energy array then lineCenterBin will be set to -1. Otherwise
+	# energyArray[lineCenterBin] < ecenterArray <= energyArray[lineCenterBin+1]
 
- 	lineCenterBin = BinarySearch.Binarysearch(energyArray, ecenterArray)
+	lineCenterBin = BinarySearch.Binarysearch(energyArray, ecenterArray)
 
- 	efirst = energyArray[0]
- 	nE = len(energyArray)
- 	elast = energyArray[nE - 1]
+	efirst = energyArray[0]
+	nE = len(energyArray)
+	elast = energyArray[nE - 1]
 
- 	# Loop around the lines
+	# Loop around the lines
 
- 	for iline in range(0, len(ecenterArray)):
- 		icen = lineCenterBin[iline]
- 		ecenter = ecenterArray[iline]
- 		lineParams = lineParamsArray[iline]
- 		lineflux = linefluxArray[iline]
+	for iline in range(0, len(ecenterArray)):
+		icen = lineCenterBin[iline]
+		ecenter = ecenterArray[iline]
+		lineParams = lineParamsArray[iline]
+		lineflux = linefluxArray[iline]
 
-
- 		# first do case of zero width line. assume for the moment that this occurs
-    	# if all the lineParams are zero
+		# first do case of zero width line. assume for the moment that this occurs
+		# if all the lineParams are zero
 		
 		deltaFunction = True
 
