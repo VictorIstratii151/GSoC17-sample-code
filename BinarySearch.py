eef54a6126620930bef298c0e822e66a2c78b0ad
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-04-02 17:48:00 +0300

    added file to store the binary search algorithm

---
+++ BinarySearch.py
@@ -0,0 +1,8 @@
+# Binary search on a RealArray to return the index of the element immediately
+# before the input value. Assumes that the RealArray is in ascending order
+
+def Binarysearch(x, y):
+	nX = len(x)
+	nY = len(y)
+
+	
--- calcLines.py
+++ calcLines.py
@@ -5,4 +5,11 @@ def calcLine(energyArray, ecenter, lineParams, lineflux, crtLevel, lineShape, qs
 	lineParamsArray = list(lineParams)
 	linefluxArray = list(lineflux)
 
-	
+
+def calcManyLines(energyArray, ecenterArray, lineParamsArray, linefluxArray, crtLevel, lineShape, qspeedy, fluxArray):
+	# Find the bins containing the line centers. This assumes that the ecenter
+ 	# array is in increasing order of energy. If the center energy falls outside
+ 	# the energy array then lineCenterBin will be set to -1. Otherwise
+ 	# energyArray[lineCenterBin] < ecenterArray <= energyArray[lineCenterBin+1]
+
+ 	lineCenterBin = []
